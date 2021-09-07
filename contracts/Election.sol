29d7b193e056f43a21f972ade684b89128af4ca4
akshat <akshatgour17@gmail.com>
2021-09-07 15:54:51 +0530

    List Candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,12 +1,20 @@
 pragma solidity >=0.4.16 <0.9.0;
 
 contract Election {
-    // Read/write candidate
-    string public candidate;
-
-    // Constructor
-    constructor() public {
-        candidate = "Candidate 1";
+    //candidate struct
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
     }
-
+    mapping (uint => Candidate) public candidates;
+    uint public candidatesCount = 0;
+    constructor() public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
 }
